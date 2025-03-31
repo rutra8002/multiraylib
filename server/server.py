a594fbcd610b8a78eefbb6a5bcffeaf6c52adba0
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2025-03-31 12:39:56 +0200

    commit spam

--- server/server.py
+++ server/server.py
@@ -1,6 +1,20 @@
 import socket
 import threading
-from client_handler import handle_client
+
+def handle_client(client_socket, server):
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            parts = data.decode('utf-8').split(',')
+            player_id = parts[0]
+            inputs = parts[1:]
+            server.update_player_position(player_id, inputs)
+        except:
+            server.clients.remove(client_socket)
+            client_socket.close()
+            break
 
 class Server:
     def __init__(self, host='0.0.0.0', port=1234):
